c7397ca17c9a29a48eb7b5cc086cd9e47f148522
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-11-08 22:14:55 +0200

    Add Cloudinary for images uploading

---
+++ app/upload/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="Image from Cloudinary"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="z9nvdnra"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+
+          const info = result.info as CloudinaryResult;
+
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => {
+          const handleOnClick = (e) => {
+            e.preventDefault();
+            open();
+          };
+
+          return (
+            <button className="btn btn-secondary" onClick={handleOnClick}>
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
