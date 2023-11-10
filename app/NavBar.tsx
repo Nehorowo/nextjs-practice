d0a9623f1a3ece5ceef79478a108ae689bcbe054
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-11-10 22:59:16 +0200

    Auth sessions, accessing session from server and client

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,28 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-slate-800 text-white p-1 space-x-3">
       <Link href="/">NextJS</Link>
       <Link href="/admin">admin</Link>
       <Link href="/users">users</Link>
-      <Link href="/api/auth/signin">Sign In</Link>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Sign In</Link>
+      )}
+      {status === "authenticated" && (
+        <div>
+          {session.user?.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Sign out
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import ProductCard from "./components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello!</h1>
+      <h1>Hello! {session && <span>{session?.user?.name}</span>}</h1>
       <Link href="/users">link to users</Link>
       <ProductCard />
       <button className="btn">BUTTON</button>
