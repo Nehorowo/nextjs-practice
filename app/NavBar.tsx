2d4610cefeabdc6b78a2e2a8d8e100619f56d84f
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-11-10 18:43:26 +0200

    Add NextAuth (AuthJS) and configure Google Provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,14 +3,11 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-800 text-white p-1">
-      <Link href="/" className="mr-5">
-        NextJS
-      </Link>
-      <Link href="/admin" className="mr-5">
-        admin
-      </Link>
+    <div className="flex bg-slate-800 text-white p-1 space-x-3">
+      <Link href="/">NextJS</Link>
+      <Link href="/admin">admin</Link>
       <Link href="/users">users</Link>
+      <Link href="/api/auth/signin">Sign In</Link>
     </div>
   );
 };
