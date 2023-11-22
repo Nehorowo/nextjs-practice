82730c4bed3a46cd15dd46f4174092b085b61e36
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-11-22 22:22:49 +0200

    Lazy loading of specific package/library

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
 import Link from "next/link";
 import { ProductCard } from "./components";
-import { useState } from "react";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "./api/auth/[...nextauth]/route";
-import dynamic from "next/dynamic";
-
-// Lazy Loading
-const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
 
 export default function Home() {
-  const [visible, setVisible] = useState(false);
   // const session = await getServerSession(authOptions);
 
   return (
@@ -21,10 +12,18 @@ export default function Home() {
       {/* <h1>Hello! {session && <span>{session?.user?.name}</span>}</h1> */}
       <Link href="/users">link to users</Link>
       <ProductCard />
-      <button className="btn" onClick={() => setVisible((prev) => !prev)}>
+      <button
+        className="btn"
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
         BUTTON
       </button>
-      {visible && <HeavyComponent />}
     </main>
   );
 }
