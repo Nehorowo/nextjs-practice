da7c5983affe104ce4ac1e01df6a2d0b05ad25cf
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-11-22 22:01:50 +0200

    Search Engine Optimization

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,34 @@ import Link from "next/link";
 import ProductCard from "./components";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
     <main>
-      <h1>
-        Hello! {session && <span>{session?.user?.name}</span>}
-      </h1>
+      <h1>Hello! {session && <span>{session?.user?.name}</span>}</h1>
       <Link href="/users">link to users</Link>
       <ProductCard />
       <button className="btn">BUTTON</button>
     </main>
   );
 }
+
+// // Overwriting Metadata for specific page
+
+// export const metadata: Metadata = {
+//   title: "...",
+// };
+
+// // Generating Metadata from backend data
+
+// export const generateMetadata = async (): Promise<Metadata> => {
+//   const product = await fetch('...')
+
+//   return {
+//     title: product.title,
+//     description: product.description
+//   }
+// };
